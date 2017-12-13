9e65ed44d9cd0fcfb9a810346855df9d6ea2d77f
berenisser <berenisser@gmail.com>
2017-12-13 14:40:46 -0300

    primer mouseout

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
 			$('.pantalla-inicio').fadeOut();
 	}, 4000);
 });
+
+$('.contenedor').mouseover(function () {
+    $('.name-food').show();
+}).mouseout(function () {
+    $('.name-food').hide();
+  
+});
+
+$('.cont').mouseover(function () {
+    $('.name-food2').show();
+}).mouseout(function () {
+    $('.name-food2').hide();
+  
+});
+

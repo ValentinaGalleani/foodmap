6ba13d16560b3e1a4abc634b86fef09afc76e2cf
berenisser <berenisser@gmail.com>
2017-12-14 10:54:17 -0300

    actializacion modales

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,38 @@
+/*vista splash*/
 $(document).ready(function(){
+  $('.vista-principal').hide();
 	setInterval(function(){
-			$('.pantalla-inicio').fadeOut();
+		$('.pantalla-inicio').fadeOut();
+    $('.vista-principal').show();
 	}, 4000);
-});
 
-$('.contenedor').mouseover(function () {
+  $('#comida-china').click(function(){
+    $('.dropdown-menu').hide();
+    $('.japon, .italia').hide();
+  });
+
+  $('#comida-japonesa').click(function() {
+    $('.dropdown-menu').hide();
+    $('.china, .italia').hide();
+  });
+
+  $('#comida-italiana').click(function() {
+    $('.dropdown-menu').hide();
+    $('.china, .japon').hide();
+  });
+
+  $('.filtro').click(function(){
+    $('.dropdown-menu').show();
+    $('.china, .japon, .italia').show();
+  });
+  //modal
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  });   
+});
+/*Mouseover*/
+
+/*$('.contenedor').mouseover(function () {
     $('.name-food').show();
 }).mouseout(function () {
     $('.name-food').hide();
@@ -16,5 +44,6 @@ $('.cont').mouseover(function () {
 }).mouseout(function () {
     $('.name-food2').hide();
   
-});
+});*/
+
 
